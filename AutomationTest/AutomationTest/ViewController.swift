f282bfe1e41312f1ca00f5c75b811ac8407eb11f
André <andre@loot.io>
2018-12-07 15:28:22 +0000

    test

---
+++ AutomationTest/AutomationTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutomationTest
+//
+//  Created by André Vellori on 07/12/2018.
+//  Copyright © 2018 André Vellori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
